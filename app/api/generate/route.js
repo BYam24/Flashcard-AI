23b8ca6af8a8f9ce2228a9f9fb8ed2792eb4abc2
Benjamin <by82@cornell.edu>
2024-08-15 00:05:15 -0400

    flashcard can be generated

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -5,7 +5,7 @@ const systemPrompt = `You are a flashcard creator. Your job is to generate flash
 Each flashcard should have a clear and concise question on one side and a detailed, accurate answer on the other. 
 The content should be tailored to the user's subject of interest, ensuring relevance and educational value.
 Focus on clarity, accuracy, and engagement. Use simple language where possible, and ensure that each flashcard encourages active recall and understanding.
-
+Only generate 9 flashcards.
 Return in the following JSON format
 {
   "flashcards": [
@@ -17,10 +17,10 @@ Return in the following JSON format
 }`;
 
 export async function POST(req) {
-  const openai = OpenAI();
+  const openai = new OpenAI();
   const data = await req.text();
 
-  const completion = await openai.chat.completion.create({
+  const completion = await openai.chat.completions.create({
     messages: [
       { role: "system", content: systemPrompt },
       { role: "user", content: data },
